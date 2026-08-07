82a6df80dad83d6e1e4444f010164457e3c0f5cc
Faisal Misbah <faisalmisbah23@gmail.com>
2026-08-07 15:58:04 +0500

    Updates

--- src/events/expiration-complete-event.ts
+++ src/events/expiration-complete-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from "./subjects"
 
 export interface ExpirationCompleteEvent {
-    subject: Subjects.ExpirationCompleteEvent;
+    subject: Subjects.ExpirationComplete;
     data: {
         orderId: string
     }
